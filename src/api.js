f88698db36be8040e6105954eaeac1acef90274c
Eric Hicks <ehicks05@yahoo.com>
2021-05-04 22:42:24 -0400

    throttle candle fetching

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import pRetry from "p-retry";
+import pThrottle from "p-throttle";
 import { formatISO, subDays } from "date-fns";
 import { API_URL } from "./constants";
 
@@ -63,25 +63,26 @@ const _getCandles = async (productId) => {
   }
 };
 
+// from https://docs.pro.coinbase.com/#rate-limits:
+// We throttle public endpoints by IP: 3 requests per second
+const throttle = pThrottle({
+  limit: 3,
+  interval: 1000,
+});
+
+const throttledFetch = throttle(async (productId) => {
+  const candles = await _getCandles(productId);
+  return { productId, candles };
+});
+
 const getCandles = async (productIds) => {
-  return (
-    await Promise.all(
-      productIds.map(async (productId) => {
-        const candles = await pRetry(() => _getCandles(productId), {
-          retries: 3,
-        });
-        return { productId, candles };
-      })
-    )
-  )
-    .flat()
-    .reduce(
-      (agg, curr) => ({
-        ...agg,
-        [curr.productId]: curr,
-      }),
-      {}
-    );
+  return (await Promise.all(productIds.map(throttledFetch))).flat().reduce(
+    (agg, curr) => ({
+      ...agg,
+      [curr.productId]: curr,
+    }),
+    {}
+  );
 };
 
 export { getCurrencies, getProducts, get24HourStats, getCandles };
