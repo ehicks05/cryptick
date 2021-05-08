5994e1c66474fb89cf5a6935e3ab8865afcf14a8
Eric Hicks <ehicks05@yahoo.com>
2021-05-08 13:09:40 -0400

    leverage lodash

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import pThrottle from "p-throttle";
 import { formatISO, subDays } from "date-fns";
 import { API_URL } from "./constants";
@@ -6,39 +7,16 @@ const CURR_URL = `${API_URL}/currencies`;
 const PROD_URL = `${API_URL}/products`;
 
 const getCurrencies = async () => {
-  return (await (await fetch(CURR_URL)).json())
-    .sort((a, b) => {
-      return a.details.sort_order - b.details.sort_order;
-    })
-    .reduce(
-      (agg, curr) => ({
-        ...agg,
-        [curr.id]: curr,
-      }),
-      {}
-    );
+  const data = await (await fetch(CURR_URL)).json();
+  return _.chain(data).sortBy(["sort_order"]).keyBy("id").value();
 };
 
 const getProducts = async () => {
-  const SUPPORTED_QUOTE_CURRENCIES = ["BTC", "USD"];
-  return (await (await fetch(PROD_URL)).json())
-    .filter((product) =>
-      SUPPORTED_QUOTE_CURRENCIES.includes(product.quote_currency)
-    )
-    .sort((a, b) => {
-      if (a.quote_currency !== b.quote_currency)
-        return a.quote_currency.localeCompare(b.quote_currency);
-      if (a.base_currency !== b.base_currency)
-        return a.base_currency.localeCompare(b.base_currency);
-      return 0;
-    })
-    .reduce(
-      (agg, curr) => ({
-        ...agg,
-        [curr.id]: curr,
-      }),
-      {}
-    );
+  const data = await (await fetch(PROD_URL)).json();
+  return _.chain(data)
+    .sortBy(["quote_currency", "base_currency"])
+    .keyBy("id")
+    .value();
 };
 
 const get24HourStats = async () => {
@@ -76,13 +54,8 @@ const throttledFetch = throttle(async (productId) => {
 });
 
 const getCandles = async (productIds) => {
-  return (await Promise.all(productIds.map(throttledFetch))).flat().reduce(
-    (agg, curr) => ({
-      ...agg,
-      [curr.productId]: curr,
-    }),
-    {}
-  );
+  const data = (await Promise.all(productIds.map(throttledFetch))).flat();
+  return _.keyBy(data, "productId");
 };
 
 export { getCurrencies, getProducts, get24HourStats, getCandles };
