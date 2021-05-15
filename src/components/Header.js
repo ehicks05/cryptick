b1ecc9603478edf05d1e4ec8fef2a7056bea392d
Eric Hicks <ehicks05@yahoo.com>
2021-05-14 22:10:04 -0400

    simplify

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,10 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = ({ title, titleClass, showSettings, setShowSettings }) => {
   return (
-    <header className="flex p-4 items-center justify-between">
-      <div className="justify-start">
-        <div title={title} className={`rounded-full h-4 w-4 ${titleClass}`} />
-      </div>
-      <div className="justify-end">
-        <div
-          className="cursor-pointer"
-          onClick={() => setShowSettings(!showSettings)}
-        >
-          {showSettings ? <FaTimes /> : <FaBars />}
-        </div>
+    <header className="flex p-4 justify-between">
+      <div title={title} className={`rounded-full h-4 w-4 ${titleClass}`} />
+      <div role="button" onClick={() => setShowSettings(!showSettings)}>
+        {showSettings ? <FaTimes /> : <FaBars />}
       </div>
     </header>
   );
