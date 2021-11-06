89f73b9be71e261c425094b8f4dc7e1a088581b3
Eric Hicks <ehicks05@yahoo.com>
2021-11-06 12:14:12 -0400

    change trade size visualization from a variable width border to variable intensity bg color, and add toggle between base/quote currency in trade size column

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const barWidth = (tradeSize) => {
-  return Math.max(Math.floor(Math.log2(tradeSize) + 8), 1);
+const normalize = (value) => {
+  if (value < 10) return 0.0;
+  if (value < 100) return 0.05;
+  if (value < 1000) return 0.1;
+  if (value < 2000) return 0.25;
+  if (value < 4000) return 0.3;
+  if (value < 8000) return 0.35;
+  if (value < 16000) return 0.4;
+  if (value < 32000) return 0.7;
+  if (value < 64000) return 0.73;
+  if (value < 128000) return 0.76;
+  return 0.8;
+};
+
+const getAlpha = (tradeSize, formattedPrice, side) => {
+  const price = Number(formattedPrice.replaceAll(",", ""));
+  const value = Number(tradeSize) * price;
+  const intensity = normalize(value);
+  const clampedIntensity = Math.min(Math.max(intensity, 0), 1);
+  // bright green seems brighter than bright red
+  const colorPerception = clampedIntensity > 0.5 && side === "buy" ? 0.7 : 1;
+  return clampedIntensity * colorPerception;
 };
 
 const SIDES = {
@@ -17,7 +37,29 @@ const SIDES = {
   },
 };
 
+const formats = {};
+const getFormat = (currency) => {
+  if (formats[currency]) return formats[currency];
+  const newFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  formats[currency] = newFormat;
+  return newFormat;
+};
+
 const History = ({ messages }) => {
+  const [sizeUnit, setSizeUnit] = useState("base");
+  const toggleSizeUnit = () =>
+    setSizeUnit(sizeUnit === "base" ? "quote" : "base");
+
+  if (messages.length === 0) return "...";
+  const [base, quote] = messages[0].productId.split("-");
+  const selectedSizeUnit = sizeUnit === "base" ? base : quote;
+  const format = getFormat(selectedSizeUnit);
+
   return (
     <div className="text-xs overflow-y-hidden">
       <table>
@@ -26,22 +68,29 @@ const History = ({ messages }) => {
             <TD colSpan="4">History</TD>
           </tr>
           <tr>
-            <TD></TD>
-            <TD>Trade Size</TD>
-            <TD>Price</TD>
-            <TD>Time</TD>
+            <TD className="text-right cursor-pointer" onClick={toggleSizeUnit}>
+              Trade Size
+            </TD>
+            <TD className="text-right">Price</TD>
+            <TD className="text-right">Time</TD>
           </tr>
         </thead>
         <tbody>
           {messages.map(({ sequence, time, side, price, last_size }) => {
-            const style = { borderLeftWidth: barWidth(last_size) };
+            const style = {
+              backgroundColor: `rgba(${
+                side === "buy" ? "0,255,150" : "255,25,0"
+              },${getAlpha(last_size, price, side)})`,
+            };
+            const tradeSize =
+              sizeUnit === "base"
+                ? last_size
+                : format.format(last_size * price.replaceAll(",", ""));
             return (
               <TR key={sequence} className={SIDES[side].highlight}>
-                <TD
-                  className={`pl-4 ${SIDES[side].borderColor} `}
-                  style={style}
-                ></TD>
-                <TD>{last_size}</TD>
+                <TD style={style} className="text-right">
+                  {tradeSize}
+                </TD>
                 <TD className={SIDES[side].textColor}>{price}</TD>
                 <TD className="opacity-50">{time}</TD>
               </TR>
