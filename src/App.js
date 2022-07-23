109936d57ec3f38b097d859f0d6c1706282d0cb2
Eric Hicks <ehicks05@yahoo.com>
2022-07-23 15:52:52 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -13,34 +13,28 @@ import DataFetcher from "DataFetcher";
 import useStore from "./store";
 
 function App() {
-  const isAppLoading = useStore(state => state.isAppLoading);
+  const isAppLoading = useStore((state) => state.isAppLoading);
 
   return (
     <Router>
       <DataFetcher />
-      {isAppLoading ?
+      {isAppLoading ? (
         <div className="flex items-center justify-center h-screen">
           <Loader type="Rings" color="#00BFFF" height={256} width={256} />
         </div>
-        :
+      ) : (
         <div className="flex flex-col h-screen">
           <Header />
           <Settings />
           <div className="flex-grow flex flex-col h-full overflow-y-hidden">
             <Routes>
-              <Route
-                path="/:productId"
-                element={<ProductDetail />}
-              />
-              <Route
-                path="/"
-                element={<Products />}
-              />
+              <Route path="/:productId" element={<ProductDetail />} />
+              <Route path="/" element={<Products />} />
             </Routes>
           </div>
           <Footer />
         </div>
-      }
+      )}
     </Router>
   );
 }
