165db568593cdb962ac36813ff0efa004ba9bcfd
Eric Hicks <ehicks05@yahoo.com>
2024-08-23 17:51:20 -0400

    border

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -10,17 +10,27 @@ const BG_COLORS = {
 	UND: 'from-[rgba(090,90,90,.15)] via-[rgba(90,90,90,.10)] to-[rgba(90,90,90,.08)] dark:to-[rgba(90,90,90,.08)]',
 } as const;
 
-const Product = ({
-	productId,
-	handle,
-}: { productId: string; handle: ReactNode }) => {
+const BORDER_COLORS = {
+	POS: 'border-green-950',
+	NEG: 'border-red-950',
+	UND: 'border-neutral-800',
+} as const;
+
+interface Props {
+	productId: string;
+	handle: ReactNode;
+}
+
+const Product = ({ productId, handle }: Props) => {
 	const { data } = use24HourStats();
 	const productStats = data?.[productId];
 
 	const isPositive = productStats ? productStats.isPositive : undefined;
 	const colorKey = isPositive === undefined ? 'UND' : isPositive ? 'POS' : 'NEG';
 	return (
-		<div className={`p-4 rounded-lg shadow bg-gradient-to-t ${BG_COLORS[colorKey]}`}>
+		<div
+			className={`p-4 rounded-lg shadow bg-gradient-to-t border ${BORDER_COLORS[colorKey]} ${BG_COLORS[colorKey]}`}
+		>
 			<div className="flex justify-between">
 				<ProductSummary productId={productId} />
 				{handle}
