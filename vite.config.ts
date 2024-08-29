00f62415a3589e60f1c11ae58f3a8681addc7945
Eric Hicks <ehicks05@yahoo.com>
2024-08-29 12:40:57 -0400

    use more defaults

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,8 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-	build: {
-		outDir: 'build',
-	},
 	server: {
 		open: true,
 		host: '0.0.0.0',
-		port: 3000,
 	},
 });
