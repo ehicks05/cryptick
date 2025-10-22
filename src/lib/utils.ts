d26061c865f44524149b80043c2cc5e692cd0ec2
Eric Hicks <ehicks05@yahoo.com>
2025-10-22 12:00:30 -0400

    getChange

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,13 +6,19 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
+export const getChange = (open: number, close: number) => {
+	return {
+		isPositive: close > open,
+		percentChange: close ? close / open - 1 : 0,
+	};
+};
+
 export const aggregateCandleStats = (candles: Candle[]) => {
 	const high = Math.max(...candles.map((o) => o.high), 0);
 	const low = Math.min(...candles.map((o) => o.low));
 	const open = candles.at(-1)?.open || 0;
 	const close = candles[0]?.close || 0;
-	const isPositive = close > open;
-	const percentChange = close ? close / open - 1 : 0;
+	const { isPositive, percentChange } = getChange(open, close);
 
 	return { open, close, low, high, isPositive, percentChange };
 };
