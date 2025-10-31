27c39abebfc445c6e4afd9e2ff8bb9f9c21818ec
Eric Hicks <ehicks05@yahoo.com>
2025-10-31 18:22:23 -0400

    lint

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -2,11 +2,10 @@
 
 import type { DialogProps } from '@radix-ui/react-dialog';
 import { Command as CommandPrimitive } from 'cmdk';
-import { Search } from 'lucide-react';
-import * as React from 'react';
-
 import { Dialog, DialogContent } from 'components/ui/dialog';
 import { cn } from 'lib/utils';
+import { Search } from 'lucide-react';
+import * as React from 'react';
 
 const Command = React.forwardRef<
 	React.ElementRef<typeof CommandPrimitive>,
--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import * as PopoverPrimitive from '@radix-ui/react-popover';
-import * as React from 'react';
-
 import { cn } from 'lib/utils';
+import * as React from 'react';
 
 const Popover = PopoverPrimitive.Root;
 
