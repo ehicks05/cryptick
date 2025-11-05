5da09cd265f4854bc8019c1f4142015db691d527
Eric Hicks <ehicks05@yahoo.com>
2025-11-05 13:02:59 -0500

    useHistoricPerformance

--- src/components/Products/Product.tsx
+++ src/components/Products/Product.tsx
@@ -1,13 +1,11 @@
 import { BG_COLORS, BORDER_COLORS, TEXT_COLORS } from 'directionalStyles';
-import { useHistoricPrices } from 'api/useCandles';
 import { useChartTimespan } from 'hooks/useStorage';
-import { cn, getChange, mergeCandles } from 'lib/utils';
+import { cn, mergeCandles } from 'lib/utils';
 import React from 'react';
-import { useThrottledPrice } from 'store';
-import { CHART_TIMESPANS, type ChartTimespan, type Direction } from 'types';
 import { formatPercent } from 'utils';
 import { ProductSummary } from './ProductSummary';
 import Chart from './SimpleChart/Chart';
+import { type Performance, useHistoricPerformance } from './useHistoricPerformance';
 import { useLiveCandles } from './useLiveCandles';
 
 interface Props {
@@ -29,76 +27,45 @@ const Product = ({ productId }: Props) => {
 		<div className={className}>
 			<ProductSummary productId={productId} />
 			<Chart productId={productId} />
-			<Performance productId={productId} />
+			<Performances productId={productId} />
 		</div>
 	);
 };
 
 const TimespanPerformance = ({
-	change,
+	change: { name, label, direction, percentChange },
 }: {
-	change: {
-		name: ChartTimespan;
-		label: string;
-		direction: Direction;
-		percentChange: number;
-	};
+	change: Performance;
 }) => {
 	const [timespan, setTimespan] = useChartTimespan();
 
 	return (
-		<div key={change.label} className="flex items-baseline gap-1">
+		<div key={label} className="flex items-baseline gap-1">
 			<span
 				className={cn('text-xs', {
-					'text-muted-foreground': change.name !== timespan,
+					'text-muted-foreground': name !== timespan,
 				})}
 			>
-				{change.label}
+				{label}
 			</span>
 			<button
 				type="button"
-				onClick={() => setTimespan(change.name)}
-				className={cn(
-					TEXT_COLORS[change.direction],
-					'text-sm font-mono cursor-pointer',
-				)}
+				onClick={() => setTimespan(name)}
+				className={cn(TEXT_COLORS[direction], 'text-sm font-mono cursor-pointer')}
 			>
-				{formatPercent(change.percentChange)}
+				{formatPercent(percentChange)}
 			</button>
 		</div>
 	);
 };
 
-const Performance = ({ productId }: { productId: string }) => {
-	const _price = useThrottledPrice(productId);
-	const cleanPriceString = _price.replace(/[$,]/g, ''); // Removes '$' and ','
-	const price = Number.parseFloat(cleanPriceString);
-
-	const { data } = useHistoricPrices([productId]);
-
-	const day1 = data?.day1Candles[productId][0]?.open || 0;
-	const day1Change = getChange(day1, Number(price));
-
-	const day7 = data?.day7Candles[productId][0]?.open || 0;
-	const day7Change = getChange(day7, Number(price));
-
-	const day30 = data?.day30Candles[productId][0]?.open || 0;
-	const day30Change = getChange(day30, Number(price));
-
-	const day365 = data?.day365Candles[productId][0]?.open || 0;
-	const day365Change = getChange(day365, Number(price));
-
-	const changes = [
-		{ name: CHART_TIMESPANS['24H'], label: 'D', ...day1Change },
-		{ name: CHART_TIMESPANS['7D'], label: 'W', ...day7Change },
-		{ name: CHART_TIMESPANS['30D'], label: 'M', ...day30Change },
-		{ name: CHART_TIMESPANS['1Y'], label: 'Y', ...day365Change },
-	];
+const Performances = ({ productId }: { productId: string }) => {
+	const { performances } = useHistoricPerformance({ productId });
 
 	return (
 		<div className="p-4 py-2 flex justify-between items-center">
-			{changes.map((change) => (
-				<TimespanPerformance key={change.name} change={change} />
+			{performances.map((performance) => (
+				<TimespanPerformance key={performance.name} change={performance} />
 			))}
 		</div>
 	);
---
+++ src/components/Products/useHistoricPerformance.ts
@@ -0,0 +1,40 @@
+import { useHistoricPrices } from 'api/useCandles';
+import { getChange } from 'lib/utils';
+import { useThrottledPrice } from 'store';
+import { CHART_TIMESPANS, type ChartTimespan, type Direction } from 'types';
+
+export interface Performance {
+	name: ChartTimespan;
+	label: string;
+	direction: Direction;
+	percentChange: number;
+}
+
+export const useHistoricPerformance = ({ productId }: { productId: string }) => {
+	const _price = useThrottledPrice(productId);
+	const cleanPriceString = _price.replace(/[$,]/g, ''); // Removes '$' and ','
+	const price = Number.parseFloat(cleanPriceString);
+
+	const { data } = useHistoricPrices([productId]);
+
+	const day1 = data?.day1Candles[productId][0]?.open || 0;
+	const day1Change = getChange(day1, Number(price));
+
+	const day7 = data?.day7Candles[productId][0]?.open || 0;
+	const day7Change = getChange(day7, Number(price));
+
+	const day30 = data?.day30Candles[productId][0]?.open || 0;
+	const day30Change = getChange(day30, Number(price));
+
+	const day365 = data?.day365Candles[productId][0]?.open || 0;
+	const day365Change = getChange(day365, Number(price));
+
+	const performances: Performance[] = [
+		{ name: CHART_TIMESPANS['24H'], label: 'D', ...day1Change },
+		{ name: CHART_TIMESPANS['7D'], label: 'W', ...day7Change },
+		{ name: CHART_TIMESPANS['30D'], label: 'M', ...day30Change },
+		{ name: CHART_TIMESPANS['1Y'], label: 'Y', ...day365Change },
+	];
+
+	return { performances };
+};
