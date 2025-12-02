4968d456a17cd162213d0e272fa765f44347b420
Eric Hicks <ehicks05@yahoo.com>
2025-12-02 12:00:05 -0500

    more options

--- src/components/Settings/ChartHeightPicker.tsx
+++ src/components/Settings/ChartHeightPicker.tsx
@@ -1,7 +1,8 @@
 import { Slider } from '@base-ui-components/react';
 import { useChartHeight } from 'hooks/useStorage';
 
-export const HEIGHTS = 'h-0 h-4 h-8 h-12 h-16 h-20 h-24 h-28 h-32 h-36 h-40';
+export const HEIGHTS =
+	'h-0 h-4 h-8 h-12 h-16 h-20 h-24 h-28 h-32 h-36 h-40 h-44 h-48 h-52 h-56 h-60 h-64';
 
 export const ChartHeightPicker = () => {
 	const [chartHeight, setChartHeight] = useChartHeight();
@@ -14,7 +15,7 @@ export const ChartHeightPicker = () => {
 					value={Number(chartHeight.slice(2))}
 					onValueChange={(e) => setChartHeight(`h-${e}`)}
 					min={0}
-					max={40}
+					max={64}
 					step={4}
 				>
 					<Slider.Control className="flex w-56 touch-none items-center py-3 select-none">
