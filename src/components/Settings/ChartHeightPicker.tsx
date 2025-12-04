247bfe09e835d80e64f9a61cf1deacd64a0866de
Eric Hicks <ehicks05@yahoo.com>
2025-12-04 18:07:11 -0500

    nit

--- src/components/Settings/ChartHeightPicker.tsx
+++ src/components/Settings/ChartHeightPicker.tsx
@@ -21,7 +21,7 @@ export const ChartHeightPicker = () => {
 					<Slider.Control className="flex w-56 touch-none items-center py-3 select-none">
 						<Slider.Track className="h-1 w-full rounded bg-gray-200 shadow-[inset_0_0_0_1px] shadow-gray-200 select-none">
 							<Slider.Indicator className="rounded bg-green-600 select-none" />
-							<Slider.Thumb className="size-4 rounded-full bg-white outline outline-gray-300 select-none has-[:focus-visible]:outline has-[:focus-visible]:outline-blue-800" />
+							<Slider.Thumb className="size-4 rounded-full bg-white outline outline-gray-300 select-none has-focus-visible:outline has-focus-visible:outline-blue-800" />
 						</Slider.Track>
 					</Slider.Control>
 				</Slider.Root>
