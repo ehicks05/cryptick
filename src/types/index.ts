0e8623fa7706f2f0e35ab80713a3a1f616698751
Eric Hicks <ehicks05@yahoo.com>
2025-12-04 14:33:03 -0500

    wip types

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface CryptickProduct {
+	base_currency: string;
+	base_increment: string;
+	display_name: string;
+	id: string;
+	quote_currency: string;
+	status: 'delisted' | 'online';
+	status_message: string;
+	trading_disabled: boolean;
+	minimumQuoteDigits: number;
+	minimumBaseDigits: number;
+}
+
+export interface CryptickCandle {
+	close: number;
+	high: number;
+	low: number;
+	open: number;
+	productId: string;
+	timestamp: number;
+	volume: number;
+}
