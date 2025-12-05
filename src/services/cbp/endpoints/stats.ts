2fd4e3bed78d1c90d872462976899380d6254299
Eric Hicks <ehicks05@yahoo.com>
2025-12-04 21:00:26 -0500

    clean

--- src/services/cbp/endpoints/stats.ts
+++ src/services/cbp/endpoints/stats.ts
@@ -7,17 +7,10 @@ import { PRODUCT_URL } from './constants';
 
 const annotateStat = ([productId, stat]: [string, BulkProductStat]) => {
 	const stat24 = stat.stats_24hour;
-	return [
-		productId,
-		{
-			...stat24,
-		},
-	];
+	return [productId, stat24];
 };
 
-export const get24HourStats = async (): Promise<
-	Record<string, Stats24Hour>
-> => {
+export const get24HourStats = async (): Promise<Record<string, Stats24Hour>> => {
 	const response = await fetch(`${PRODUCT_URL}/stats`);
 	const stats: BulkProductStats = await response.json();
 	return Object.fromEntries(Object.entries(stats).map(annotateStat));
