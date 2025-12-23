55f4d827bc6a8eb4b0ee80c6de2f2f99f6c9e801
Eric Hicks <ehicks05@yahoo.com>
2025-12-22 21:11:05 -0500

    wip

---
+++ src/services/kraken/ohlc.ts
@@ -0,0 +1,62 @@
+import { CandleGranularity } from "services/cbp/types/product";
+import { keyByProductId } from "services/utils";
+import type { CryptickCandle } from "types";
+import { client } from "./client";
+import { throttle } from "./throttle";
+import type { Ohlc, OhlcResponse } from "./types";
+
+const secondsToKrakenInterval = {
+  [CandleGranularity.ONE_MINUTE]: "1",
+  [CandleGranularity.FIVE_MINUTES]: "5",
+  [CandleGranularity.FIFTEEN_MINUTES]: "15",
+  [CandleGranularity.ONE_HOUR]: "60",
+  [CandleGranularity.SIX_HOURS]: "360",
+  [CandleGranularity.ONE_DAY]: "1440",
+};
+
+const toCryptickCandle = (candle: Ohlc, productId: string): CryptickCandle => ({
+  productId: `kraken:${productId}`,
+  timestamp: Number(candle[0]),
+  open: Number(candle[1]),
+  high: Number(candle[2]),
+  low: Number(candle[3]),
+  close: Number(candle[4]),
+  volume: Number(candle[6]),
+});
+
+interface Params {
+  pair: string;
+  interval: CandleGranularity;
+  since?: number;
+}
+
+const _ohlc = async ({ pair, interval, since }: Params) => {
+  const response = await client({
+    path: "/OHLC",
+    params: new URLSearchParams({
+      pair,
+      interval: secondsToKrakenInterval[interval],
+      since: since ? String(since * 1000) : "",
+    }).toString(),
+  });
+  const { result }: OhlcResponse = await response.json();
+  const items = result[pair];
+  return items.map((item) => toCryptickCandle(item, pair)).toReversed();
+};
+
+export const ohlc = throttle(_ohlc);
+
+interface ParamsMulti extends Omit<Params, "symbol"> {
+  pairs: string[];
+}
+
+export const getKlinesForProducts = async (_params: ParamsMulti) => {
+  const { pairs, pair: _pair, ...params } = _params;
+  const toPromise = async (pair: string) => {
+    const result = await ohlc({ pair, ...params });
+    return { productId: `kraken:${pair}`, candles: result };
+  };
+  const data = await Promise.all(pairs.map(toPromise));
+
+  return keyByProductId(data.flat());
+};
--- src/services/kraken/types.ts
+++ src/services/kraken/types.ts
@@ -1,55 +1,69 @@
 export interface Asset {
-	aclass: 'currency';
-	altname: string;
-	decimals: number;
-	display_decimals: number;
-	status:
-		| 'enabled'
-		| 'deposit_only'
-		| 'withdrawal_only'
-		| 'funding_temporarily_disabled';
-	collateral_value?: number;
-	margin_rate?: number;
+  aclass: "currency";
+  altname: string;
+  decimals: number;
+  display_decimals: number;
+  status:
+    | "enabled"
+    | "deposit_only"
+    | "withdrawal_only"
+    | "funding_temporarily_disabled";
+  collateral_value?: number;
+  margin_rate?: number;
 }
 
 export interface AssetsResponse {
-	error: string[];
-	results: Record<string, Asset>;
+  error: string[];
+  results: Record<string, Asset>;
 }
 
 export interface AssetPair {
-	altname: string;
-	wsname: string;
-	aclass_base: 'currency';
-	base: string;
-	aclass_quote: 'currency';
-	quote: string;
-	cost_decimals: number;
-	pair_decimals: number;
-	lot_decimals: number;
-	lot_multiplier: number;
-	leverage_buy: number[];
-	leverage_sell: number[];
-	fees: [number, number][];
-	fees_maker: [number, number][];
-	fee_volume_currency: string;
-	margin_call: number;
-	margin_stop: number;
-	ordermin: string;
-	costmin: string;
-	tick_size: string;
-	status: 'online' | 'cancel_only' | 'post_only' | 'limit_only' | 'reduce_only';
+  altname: string;
+  wsname: string;
+  aclass_base: "currency";
+  base: string;
+  aclass_quote: "currency";
+  quote: string;
+  cost_decimals: number;
+  pair_decimals: number;
+  lot_decimals: number;
+  lot_multiplier: number;
+  leverage_buy: number[];
+  leverage_sell: number[];
+  fees: [number, number][];
+  fees_maker: [number, number][];
+  fee_volume_currency: string;
+  margin_call: number;
+  margin_stop: number;
+  ordermin: string;
+  costmin: string;
+  tick_size: string;
+  status: "online" | "cancel_only" | "post_only" | "limit_only" | "reduce_only";
 }
 
 export interface AssetPairsResponse {
-	error: string[];
-	results: Record<string, AssetPair>;
+  error: string[];
+  results: Record<string, AssetPair>;
 }
 
 // [time, open, high, low, close, vwap, volume, count]
-export type Ohlc = [number, string, string, string, string, string, string, number];
+export type Ohlc = [
+  number,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  number,
+];
+
+interface OhlcResult {
+  // last: number;
+  [key: string]: Ohlc[];
+}
 
 export interface OhlcResponse {
-	error: string[];
-	result: Record<string, Ohlc[]>;
+  error: string[];
+  result: OhlcResult;
 }
