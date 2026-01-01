2a45b2eddbf38bd2525f2b144b16daa274844a0e
Eric Hicks <ehicks05@yahoo.com>
2026-01-01 13:35:26 -0500

    size

--- src/components/ExchangeIcon.tsx
+++ src/components/ExchangeIcon.tsx
@@ -3,31 +3,31 @@ import type { Exchange } from 'types';
 import Kraken from '../../public/kraken-logo.svg?react';
 import Kraken2 from '../../public/kraken-logo-2.svg?react';
 
-const size = 12;
-
 const CbIcon = () => (
 	<TbBrandCoinbase
 		title="coinbase"
-		size={size}
-		className="fill-blue-500 stroke-blue-500 stroke-1"
+		className="w-full h-full fill-blue-500 stroke-blue-500 stroke-1"
 	/>
 );
 const BiIcon = () => (
 	<TbBrandBinance
 		title="binance.us"
-		size={size}
-		className="fill-yellow-500 stroke-yellow-500 stroke-1"
+		className="w-full h-full fill-yellow-500 stroke-yellow-500 stroke-1"
 	/>
 );
-const KrIcon = () => <Kraken title="kraken" className="size-4 stroke-1" />;
+const KrIcon = () => <Kraken title="kraken" className="w-full h-full" />;
 const KrIconAlt = () => (
-	<Kraken2 title="kraken" className="size-3 fill-violet-500 stroke-1" />
+	<Kraken2 title="kraken" className="w-full h-full fill-violet-500 stroke-1" />
 );
 
-export const ExchangeIcon = ({ name }: { name: Exchange }) => {
-	if (name === 'coinbase') return <CbIcon />;
-	if (name === 'binance') return <BiIcon />;
-	if (name === 'kraken') return <KrIcon />;
+const EXCHANGE_ICONS = {
+	coinbase: CbIcon,
+	binance: BiIcon,
+	kraken: KrIcon,
+} as const;
 
-	return <CbIcon />;
+export const ExchangeIcon = ({ name }: { name: Exchange; size?: number }) => {
+	const Icon = EXCHANGE_ICONS[name];
+
+	return <Icon />;
 };
