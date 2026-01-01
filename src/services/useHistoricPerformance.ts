c08b22e2c2a42034026b674495afff3ce782bc40
Eric Hicks <ehicks05@yahoo.com>
2026-01-01 14:39:15 -0500

    fix

--- src/services/useHistoricPerformance.ts
+++ src/services/useHistoricPerformance.ts
@@ -1,4 +1,5 @@
 import { getChange } from 'lib/math';
+import { useThrottledPrice } from 'store';
 import { CHART_TIMESPANS, type ChartTimespan, type Direction } from 'types';
 import { useHistoricPrices } from './useHistoricPrices';
 
@@ -10,21 +11,23 @@ export interface Performance {
 }
 
 export const useHistoricPerformance = ({ productId }: { productId: string }) => {
+	const _price = useThrottledPrice(productId);
+	const cleanPriceString = _price.replace(/[$,]/g, ''); // Removes '$' and ','
+	const price = Number.parseFloat(cleanPriceString);
+
 	const { data } = useHistoricPrices([productId]);
 
-	const latest = data?.latestCandles[productId]?.[0]?.open || 0;
-
 	const day1 = data?.day1Candles[productId]?.[0]?.open || 0;
-	const day1Change = getChange(day1, latest);
+	const day1Change = getChange(day1, Number(price));
 
 	const day7 = data?.day7Candles[productId]?.[0]?.open || 0;
-	const day7Change = getChange(day7, latest);
+	const day7Change = getChange(day7, Number(price));
 
 	const day30 = data?.day30Candles[productId]?.[0]?.open || 0;
-	const day30Change = getChange(day30, latest);
+	const day30Change = getChange(day30, Number(price));
 
 	const day365 = data?.day365Candles[productId]?.[0]?.open || 0;
-	const day365Change = getChange(day365, latest);
+	const day365Change = getChange(day365, Number(price));
 
 	const performances: Performance[] = [
 		{ name: CHART_TIMESPANS['24H'], label: 'D', ...day1Change },
