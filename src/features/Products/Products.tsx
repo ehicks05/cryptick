f00e4cef2cc95371eb9a27f922ce416311363914
Eric Hicks <ehicks05@yahoo.com>
2026-09-18 13:31:37 -0400

    remove hook

--- src/features/Products/Products.tsx
+++ src/features/Products/Products.tsx
@@ -1,5 +1,5 @@
+import { cn } from 'cn';
 import React from 'react';
-import { useScreen } from 'usehooks-ts';
 import { useProductIds } from '@/hooks/useStorage';
 import { DndProvider, SortableItem } from './Dnd';
 import Product from './Product';
@@ -7,15 +7,15 @@ import Product from './Product';
 const Products = () => {
 	const { productIds, setProductIds } = useProductIds();
 
-	const { width = 0 } = useScreen();
-	const minColumnWidth = width < 400 ? width - 16 - 16 - 16 - 16 : 380;
-	const style = {
-		gridTemplateColumns: `repeat( auto-fill, minmax(${minColumnWidth}px, 1fr))`,
-	};
-
 	return (
 		<DndProvider ids={productIds} setIds={setProductIds}>
-			<div className="w-full grid gap-2" style={style}>
+			<div
+				className={cn(
+					'w-full grid gap-2',
+					'grid-cols-[repeat(auto-fill,minmax(320px,1fr))]',
+					'[min-w-400px]:grid-cols-[repeat(auto-fill,minmax(380px,1fr))]',
+				)}
+			>
 				{productIds.map((productId, i) => {
 					return (
 						<SortableItem key={productId} id={productId} index={i}>
